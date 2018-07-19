60cc52799844e6323d01061a9c13c6549cbac119
VALDINGER <svalding@kent.edu>
2018-07-19 00:02:44 -0400

    Reworked files and deleted unneeded ones.

---
+++ Invoke-EmployeeSeparation -- Pipeline.ps1
@@ -0,0 +1,57 @@
+﻿Function Invoke-EmployeeSeparation {
+    [cmdletBinding()]
+    Param(
+    [parameter(Mandatory,Position=0)]
+    [string]$CSVPath,
+    [parameter(Position=1)]
+    [switch]$RemoveHomeDir
+    )
+
+    $Units = Get-ADOrganizationalUnit -Filter * | Where {$_.DistinguishedName -notmatch "FormerEmployees"} | Select -ExpandProperty DistinguishedName
+
+    Foreach($ou in $Units){
+
+        Write-Verbose "Working in $ou"
+        
+        $users = Get-ADUser -Filter {Enabled -eq $false} -Properties DistinguishedName, mail, SamAccountName, memberof -SearchBase $ou
+
+        Write-Verbose "Found $($users.Count) disabled users in $ou, processing"
+        
+        $users |
+    
+        ForEach-Object {
+
+            Foreach ($group in $_.memberOf) {
+                If($group -notmatch 'Domain Users'){
+        
+                Remove-ADGroupMember -Identity $group -Members $_.SamAccountName -Confirm:$false -Verbose
+        
+                }
+        
+            }
+
+                Add-ADGroupMember -Identity Disabled -Members $_.SamAccountName -Verbose
+            
+
+                Move-ADObject -Identity $_.DistinguishedName -TargetPath 'OU=FormerEmployees,DC=testlab,DC=ad' -Verbose
+            
+                If($RemoveHomeDir){
+                
+                    Remove-Item -Path "C:\Home\$($_.SamAccountName)" -Force -Confirm:$false -Verbose
+                
+                }
+
+                [pscustomobject]@{
+                    'User'           = $_.SamAccountName
+                    'Status'         = 'Disabled'
+                    'Email Address'  = $_.mail
+                    'Groups Removed' = $true
+                } | Export-CSV -Path $CSVPath -NoTypeInformation -Append -Verbose #User loop 
+            
+        }#pipeline
+    
+        Write-Verbose "Finished processing on $ou"
+
+    }#ou
+
+}#function
---
+++ New-EmployeeOnboarding.ps1
@@ -0,0 +1,62 @@
+﻿Function New-EmployeeOnboarding {
+    [cmdletBinding()]
+    Param(
+    [Parameter(Mandatory,Position=0)]
+    [String]$CSVFile,
+    [Parameter(Mandatory=$false,Position=1)]
+    [Switch]$ProvisionHomeDrive,
+    [Parameter(Mandatory=$false,Position=2)]
+    [Switch]$ProvisionExchange
+    )
+
+    If(!(Get-Module -ListAvailable -Name NTFSSecurity)){
+        Install-Module NTFSSecurity -Force
+
+    }
+
+    Else{
+
+        Import-Module NTFSSecurity
+
+    }
+
+    $users = Import-CSV $csvfile
+    
+    Foreach($user in $users){
+
+        $props = @{
+                'Name' = "$($user.FirstName).$($user.LastName)"
+                'GivenName' = $user.FirstName
+                'Surname' = $user.LastName
+                'SamAccountName' = "$($user.FirstName).$($user.LastName)"
+                'Email' = "$($user.Firstname).$($user.Lastname)@testlab.ad"
+                'HomeDrive' = "\\client\home\$($user.FirstName).$($user.LastName)"
+                'Enabled' = $true
+                'AccountPassword' = $("P@ssw0rd!" | ConvertTo-SecureString -AsPlainText -Force)
+                'Path' = "OU=Employees,DC=testlab,DC=ad"
+
+                }
+        New-ADUser @props
+        
+        If($ProvisionHomeDrive){
+            If(!(Test-Path "C:\Home\$($user.FirstName).$($user.LastName)")){
+                
+                New-Item -ItemType Directory -Path C:\Home -Name "$($user.FirstName).$($user.LastName)" >$null
+                
+            }
+
+            Add-NTFSAccess -Path "C:\Home\$($user.FirstName).$($user.LastName)" -Account "TESTLAB\$($user.FirstName).$($user.LastName)" -AccessRights FullControl >$null
+    
+    
+    
+        }
+
+
+        If($ProvisionExchange){
+        
+        #add code to enable mailox in exchange
+     
+        }
+   }
+
+}
